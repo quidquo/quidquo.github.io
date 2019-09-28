46be16a78bf3e1c5b2667913d7ff258c3f178019
Tyler <realpsych22@gmail.com>
2019-09-28 14:35:42 -0500

    jekyll_take2

--- src/utilities/url-builder.js
+++
@@ -1,7 +0,0 @@
-export const committeeUrl = () => {
-    return `https://data.iowa.gov/resource/5dtu-swbk.json`;
-};
-
-export const contributionsUrl = () => {
-    return `https://data.iowa.gov/resource/fbcf-6uqr.json`;
-};
